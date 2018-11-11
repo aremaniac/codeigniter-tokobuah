07f94a11d4068aca9c2db7542f5f635c0e61b691
Bintang Romadhon <bintang.arema@gmail.com>
2018-11-12 01:13:52 +0700

    CRUD

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -38,10 +38,11 @@ class Product_model extends CI_Model
   }
 
   public function save(){
-    $post = $this->input->post();
+    $post             = $this->input->post();
     $this->product_id = uniqid();
-    $this->name = $post["name"];
-    $this->price = $post["price"];
+    $this->name       = $post["name"];
+    $this->price      = $post["price"];
+    $this->image      = $this->_uploadImage();
     $this->description = $post["description"];
     $this->db->insert($this->_table, $this);
   }
@@ -52,13 +53,51 @@ class Product_model extends CI_Model
     $this->name = $post["name"];
     $this->price = $post["price"];
     $this->description = $post["description"];
+
+    if (!empty($_FILES["image"]["name"])) {
+      $this->image = $this->_uploadImage();
+    } else {
+      $this->image = $post["old_image"];
+    }
+
     $this->db->update($this->_table, $this, array('product_id' => $post['id']));
   }
 
   public function delete($id)
   {
+    $this->_deleteImage($id);
     return $this->db->delete($this->_table, array("product_id" => $id));
   }
+
+	public function _uploadImage()
+	{
+		$config['upload_path']          = './upload/product/';
+		$config['allowed_types']        = 'gif|jpg|png';
+		$config['file_name']            = $this->product_id;
+		$config['overwrite']						= true;
+		$config['max_size']             = 1024; // 1MB
+		// $config['max_width']            = 1024;
+		// $config['max_height']           = 768;
+
+		$this->load->library('upload', $config);
+
+		if ($this->upload->do_upload('image')) {
+			return $this->upload->data("file_name");
+		}
+
+			return "default.jpg";
+	}
+
+  private function _deleteImage($id)
+  {
+    $product = $this->getById($id);
+    if ($product->image != "default.jpg") {
+      $filename = explode(".", $product->image)[0];
+
+      return array_map('unlink', glob(FCPATH."upload/product/$filename.*"));
+    }
+  }
+
 }
 
 ?>
